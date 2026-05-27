7e6caf7bcb8404fb99ed3d693591261fffc1490b
hjttu <1700086986@qq.com>
2026-05-27 22:54:14 +0800

    Fix CBF adj batching for recurrent buffer

--- official_G_MAT_Lagr/onpolicy/utils/gs_buffer.py
+++ official_G_MAT_Lagr/onpolicy/utils/gs_buffer.py
@@ -1015,7 +1015,18 @@ class GSReplayBuffer(object):
             .transpose(1, 2, 0, 3, 4)
             .reshape(-1, *self.node_obs.shape[3:])
         )
-        adj = self.adj[:-1].transpose(1, 2, 0, 3, 4).reshape(-1, *self.adj.shape[3:])
+        if self.adj.ndim == 6:
+            adj = (
+                self.adj[:-1]
+                .transpose(1, 2, 0, 3, 4, 5)
+                .reshape(-1, *self.adj.shape[3:])
+            )
+        else:
+            adj = (
+                self.adj[:-1]
+                .transpose(1, 2, 0, 3, 4)
+                .reshape(-1, *self.adj.shape[3:])
+            )
         agent_id = _cast(self.agent_id[:-1])
         share_agent_id = _cast(self.share_agent_id[:-1])
         actions = _cast(self.actions)
